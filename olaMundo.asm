2f4155cbe43dae36eb4f48e6ab5b0d210c7ba584
Ana Cristina <68512242+momoyo-droid@users.noreply.github.com>
2021-05-01 16:44:05 -0300

    Delete olaMundo.asm

--- olaMundo.asm
+++
@@ -1,9 +0,0 @@
-.data #especificação de variáveis. entrada de dados na memória principal
-	msg: .asciiz "Olá, mundo!" #mensagem a ser exibida para o usuário. 
-.text #área para instruções do programa
-	li $v0, 4 #instrução para impressão de string.
-	la $a0, msg #indica o endereço em que está a mensagem.
-	syscall #imprima
-	li $v0, 10 #encerrar o programa
-	syscall #faça o encerramento do programa
-	
