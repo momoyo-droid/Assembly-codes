ab85eebad8812e5982a3969dc12dde63f77d7f39
Ana Cristina <68512242+momoyo-droid@users.noreply.github.com>
2021-05-04 15:52:06 -0300

    integer subt

---
+++ sub.asm
@@ -0,0 +1,9 @@
+.text #instruções
+	
+	li $t0, 300 #atribuindo um valor a $t0
+	li $t1, 250 #atribuindo um valor a $t1
+	
+	sub $s0, $t0, $t1 #subtraindo o valor de $t0 e $t1 e atribuindo ao registrador $s0
+	
+	subi $s1, $t0, 100 #subtraindo o valor 100 ao registrador $t0 e atribuindo o resultado ao registrador $s1
+	subi $s2, $s0, 200 #subtraindo o valor 200 no registrador $s0 que já possui a subtração dos regs. $t0 e $t1.
